9b9647e0cb7c1b1b774666c38b3e8adbf67f1b8b
Ivan Malkov <ozonep@yandex.ru>
2017-12-03 21:15:43 +0300

    Homework1

---
+++ src/index.js
@@ -0,0 +1,84 @@
+/* ДЗ 1 - Функции */
+
+/*
+ Задание 1:
+
+ Функция должна принимать один аргумент и возвращать его
+ */
+function returnFirstArgument(arg) {
+    return arg;
+}
+
+/*
+ Задание 2:
+
+ Функция должна принимать два аргумента и возвращать сумму переданных значений
+ Значение по умолчанию второго аргумента должно быть 100
+ */
+function defaultParameterValue(a, b) {
+    if (!b) {
+        return a + 100;
+    }
+
+    return a + b;
+}
+
+/*
+ Задание 3:
+
+ Функция должна возвращать все переданные в нее аргументы в виде массива
+ Количество переданных аргументов заранее неизвестно
+ */
+function returnArgumentsArray() {
+    let arr = [];
+
+    for (let i=0; i<arguments.length; i++) {
+        arr.push(arguments[i]);
+    }
+
+    return arr;
+}
+
+/*
+ Задание 4:
+
+ Функция должна принимать другую функцию и возвращать результат вызова переданной функции
+ */
+function returnFnResult(fn) {
+    return fn();
+}
+
+/*
+ Задание 5:
+
+ Функция должна принимать число (значение по умолчанию - 0) и возвращать функцию (F)
+ При вызове F, переданное число должно быть увеличено на единицу и возвращено из F
+ */
+function returnCounter(number) {
+    if (!number) {
+        number = 0
+    }
+
+    return () => number += 1;
+}
+
+/*
+ Задание 6 *:
+
+ Функция должна принимать другую функцию (F) и некоторое количество дополнительных аргументов
+ Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
+ */
+function bindFunction(fn) {
+    let args = Array.prototype.slice.call(arguments);
+
+    return fn.bind.apply(fn, args);
+}
+
+export {
+    returnFirstArgument,
+    defaultParameterValue,
+    returnArgumentsArray,
+    returnFnResult,
+    returnCounter,
+    bindFunction
+}
